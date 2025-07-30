e3fda3fc312e9cc1cec64e3e5cd01c4b446e4dd1
wai-yanphyo <waiyanphyo830@gmail.com>
2025-07-30 17:34:43 +0100

    adding some elements to give the information for customers if they donot chhose any item. and I add some necessaries fn inside the component in  cart.ts to call back from html page.

--- B9IS124_20048955/src/app/cart/cart.page.ts
+++ B9IS124_20048955/src/app/cart/cart.page.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DataService, CartItem, MenuItem } from '../services/data.service';
+import { Haptics  } from '@capacitor/haptics';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.page.html',
   styleUrls: ['./cart.page.scss'],
+  standalone: false 
 })
-export class CartPage implements OnInit {
+export class CartPage implements OnInit,OnDestroy {
 
-  constructor() { }
+  cartItems: CartItem[] = [];
+  cartTotal: number = 0;
+
+  isModalOpen: boolean = false;
+  modalTitle: string = '';
+  modalMessage: string = '';
+
+  private cartSubscription!: Subscription;
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+     this.cartSubscription = this.dataService.cart$.subscribe(cart => {
+      this.cartItems = cart;
+      this.cartTotal = this.dataService.getCartTotal();
+    });
+  }
+
+    ngOnDestroy() {
+    if (this.cartSubscription) {
+      this.cartSubscription.unsubscribe();
+    }
+  }
+  
+  addToCart(item: CartItem): void {
+    this.dataService.addToCart(item as MenuItem);
+  }
+
+removeFromCart(itemId: number): void {
+    this.dataService.removeFromCart(itemId);
+  }
+
+
+ 
+
+   showModal(title: string, message: string): void {
+    this.modalTitle = title;
+    this.modalMessage = message;
+    this.isModalOpen = true;
   }
 
 }
