da91774a5b3f6fc1f0e90f61217a12983a2d1b23
wai-yanphyo <waiyanphyo830@gmail.com>
2025-07-30 17:14:36 +0100

    adding new page cart -> ionic generate page cart

--- B9IS124_20048955/src/app/app-routing.module.ts
+++ B9IS124_20048955/src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'menu',
     loadChildren: () => import('./menu/menu.module').then( m => m.MenuPageModule)
   },
+  {
+    path: 'cart',
+    loadChildren: () => import('./cart/cart.module').then( m => m.CartPageModule)
+  },
 ];
 
 @NgModule({
