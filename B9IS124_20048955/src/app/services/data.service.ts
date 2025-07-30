b1c4abfb9df39315df9a90cec612988f25abc17c
wai-yanphyo <waiyanphyo830@gmail.com>
2025-07-30 14:53:37 +0100

    defining interface

--- B9IS124_20048955/src/app/services/data.service.ts
+++ B9IS124_20048955/src/app/services/data.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
 
+
+export interface MenuItem {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  imageUrl: string;
+  category: string;
+  clickCount?: number;
+}
+
+
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class DataService {
 
   constructor() { }
