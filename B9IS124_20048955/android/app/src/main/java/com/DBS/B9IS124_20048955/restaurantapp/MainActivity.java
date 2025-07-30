79104f76b617b112eb31455c604766fea8b7be9e
wai-yanphyo <waiyanphyo830@gmail.com>
2025-07-30 23:39:41 +0100

    add packages

---
+++ B9IS124_20048955/android/app/src/main/java/com/DBS/B9IS124_20048955/restaurantapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.DBS.B9IS124_20048955.restaurantapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- B9IS124_20048955/capacitor.config.ts
+++ B9IS124_20048955/capacitor.config.ts
@@ -1,9 +1,25 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.DBS.B9IS124_20048955.restaurantapp',
   appName: 'B9IS124_20048955',
-  webDir: 'www'
+  webDir: 'www',
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: "#b0f9beff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true
+    }
+  }
 };
 
 export default config;
