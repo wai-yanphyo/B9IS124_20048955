ec53063e2ee263dc9ba154227ca82b8e13742841
wai-yanphyo <waiyanphyo830@gmail.com>
2025-07-30 14:41:16 +0100

    runing ionic start commomad to build a framework to develop

---
+++ B9IS124_20048955/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'B9IS124_20048955',
+  webDir: 'www'
+};
+
+export default config;
