42a3ab95547e05db2270cecd62f35472e18a768d
wai-yanphyo <waiyanphyo830@gmail.com>
2025-07-31 22:12:29 +0100

    fixing some issues in icon and splash

--- B9IS124_20048955/capacitor.config.ts
+++ B9IS124_20048955/capacitor.config.ts
@@ -4,21 +4,17 @@ const config: CapacitorConfig = {
   appId: 'com.DBS.B9IS124_20048955.restaurantapp',
   appName: 'B9IS124_20048955',
   webDir: 'www',
-  bundledWebRuntime: false,
   plugins: {
     SplashScreen: {
       launchShowDuration: 3000,
-      launchAutoHide: true,
       backgroundColor: "#b0f9beff",
-      androidSplashResourceName: "splash",
-      androidScaleType: "CENTER_CROP",
       showSpinner: true,
       androidSpinnerStyle: "large",
       iosSpinnerStyle: "small",
       spinnerColor: "#999999",
       splashFullScreen: true,
-      splashImmersive: true
-    }
+      splashImmersive: true,
+    },
   }
 };
 
